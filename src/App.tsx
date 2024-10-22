8fe6b293d6349f80ef65e34a651ae2d6fd84ad4b
KHbrn <karolinahbrn@gmail.com>
2024-10-22 10:18:11 +0200

    Set up basic routing for the app with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import Body from './components/Body';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-green-700">
-      Hello world!
-    </h1>
+    <Body />     
   )
 }
