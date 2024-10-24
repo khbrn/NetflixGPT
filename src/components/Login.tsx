07caac3ddade47efc1626723ccb11ac4fcbb6202
KHbrn <karolinahbrn@gmail.com>
2024-10-24 18:15:20 +0200

    Implement LoginForm

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import LoginBackground from "./LoginBackground";
+import LoginForm from "./LoginForm";
 
 const Login: React.FC = () => {
     return (
-        <div className="relative">
+        <div className="relative h-screen w-screen">
             <Header />
             <LoginBackground />
+            <LoginForm />
         </div>
     )
 }
