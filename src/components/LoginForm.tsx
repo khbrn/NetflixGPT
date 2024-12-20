d03d2569c5a292d0f1498ace644a269578ad4eee
KHbrn <karolinahbrn@gmail.com>
2024-12-20 13:09:35 +0100

    Add unit tests for LoginForm

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -52,7 +52,7 @@ const LoginForm = () => {
             <button type='submit' onClick={submitFormHandler} className='p-2 m-3 rounded bg-red-700 text-white'>{formType}</button>
             <p className='text-red-500 pl-3'>{authErrorMsg}</p>
             <p className='text-gray-400 m-3'>{isSignInForm? NETFLIX_NEW_USER_TEXT: NETFLIX_CURRENT_USER_TEXT}</p>
-            <p className='text-white ml-3 cursor-pointer' onClick={ changeFormTypeHandler }>{isSignInForm ? SIGN_UP_TEXT : LOGIN_TEXT}</p>
+            <p className='text-white ml-3 cursor-pointer' role='button' onClick={ changeFormTypeHandler }>{isSignInForm ? SIGN_UP_TEXT : LOGIN_TEXT}</p>
         </form>
     )
 }
--- src/components/__tests__/Browse.test.tsx
+++ src/components/__tests__/Browse.test.tsx
@@ -1,9 +1,24 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 
 import Browse from '../Browse';
+import Login from '../Login';
 
 describe('Browse', () => {
+    it('renders the Browser component at /browse path', () => {
+    render(
+      <MemoryRouter initialEntries={['/browse']}>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/browse' element={<Browse />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByRole('button', { name: /sign out/i })).toBeInTheDocument();
+    });
+  
   it('renders header with sign out button', () => {
     render(<Browse />)
     
